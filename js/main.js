af0fa92d1fc4059c4137d43af43cfe4264d68f98
Ivan Grushevich <githubmail911@gmail.com>
2017-12-11 00:26:38 +0200

    add animated asteroids

--- js/main.js
+++ js/main.js
@@ -15,9 +15,9 @@ function preload() {
     game.load.image('space', 'assets/img/space.jpg');
     game.load.image('ground', 'assets/img/ground.png');
     game.load.spritesheet('rocket', 'assets/img/rocket-spritesheet.png', 50, 50);
+    game.load.spritesheet('asteroid', 'assets/img/asteroid-spritesheet.png', 75, 75);
     game.load.image('fuelBar', 'assets/img/fuelBar.png');
     game.load.image('fuelCan', 'assets/img/fuelCan.png');
-    game.load.image('asteroid', 'assets/img/asteroid.png');
     game.load.image('clouds', 'assets/img/clouds.png');
 }
 
@@ -27,6 +27,7 @@ let cursors;
 let fuelBar;
 let fuelCans;
 let asteroids;
+let collision;
 
 function create() {
     // common game settings
@@ -51,9 +52,7 @@ function create() {
     rocket.angle = -90;
     rocket.body.collideWorldBounds = true;
     rocket.body.setCircle(10, 15, 15);
-
-    // create animation for the rocket
-    rocket.animations.add('move', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 12, true);
+    rocket.animations.add('move', makeArray(16), 12, true);
     
     // create controls
     cursors = game.input.keyboard.createCursorKeys();
@@ -68,8 +67,10 @@ function create() {
     for (let i = 0; i < 20; i++ ) {
         let asteroid = asteroids.create( game.world.randomX, game.world.randomY - (CONFIGS.skyHeight + 100), 'asteroid' );
         let size = 0.5 + Math.random();
-        asteroid.body.setCircle(35, 0, 0);
+        asteroid.body.setCircle(30, 7, 8);
         asteroid.scale.setTo(size);
+        asteroid.animations.add('spin', makeArray(48), 8, true);
+        asteroid.animations.play('spin');
     }
     asteroids.setAll('body.immovable', true);
 
@@ -77,7 +78,7 @@ function create() {
     fuelCans = game.add.group();
     fuelCans.enableBody = true;
     for (let i = 0; i < 40; i++ ) {
-        let fuelCan = fuelCans.create( game.world.randomX, game.world.randomY - 200, 'fuelCan' );
+        let fuelCan = fuelCans.create( game.world.randomX, game.world.randomY - 100, 'fuelCan' );
         fuelCan.body.setSize(46, 58, -9, -9);
     }
 
@@ -95,12 +96,12 @@ function create() {
 }
 function update() {
 
-    // animation
+    // animations
     rocket.animations.play('move');
 
     // collisions
     game.physics.arcade.collide(rocket, ground);
-    game.physics.arcade.collide(rocket, asteroids);
+    collision = game.physics.arcade.collide(rocket, asteroids);
 
     // enable controls
     rocket.body.acceleration.set(0);
@@ -156,4 +157,7 @@ class FuelBar extends Phaser.Group {
         }
         this.bar.scale.setTo(this.fuelAmount, 3);
     }  
+}
+function makeArray(n) {
+    return Array(n).fill().map((el,i) => i);
 }
