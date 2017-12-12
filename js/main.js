dcc004519e404952ca9510fa067c6840d2968e22
Ivan Grushevich <githubmail911@gmail.com>
2017-12-13 00:54:17 +0200

    change fuel cans, add animation to them

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const CONFIGS = {
     mapHeight: 12000,
     skyHeight: 1000,
     groundHeight: 50,
-    rocketMaxVelocity: 300
+    rocketMaxVelocity: 300,
+    fuelIncreaseAmount: 10
 };
 
 const gameField = document.querySelector('.game-field');
@@ -22,7 +23,7 @@ function preload() {
     game.load.spritesheet('rocket', 'assets/img/rocket-spritesheet.png', 50, 50);
     game.load.spritesheet('asteroid', 'assets/img/asteroid-spritesheet.png', 150, 150);
     game.load.image('fuelBar', 'assets/img/fuelBar.png');
-    game.load.image('fuelCan', 'assets/img/fuelCan.png');
+    game.load.spritesheet('fuelCan', 'assets/img/fuelCan-spritesheet.png', 35, 40);
     game.load.image('clouds', 'assets/img/clouds.png');
 
     game.load.image('mercury', 'assets/img/planets/1-mercury.png');
@@ -51,15 +52,15 @@ let explosionSound;
 
 
 function create() {
-    // common game settings
+    // general game settings
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.world.setBounds(0, 0, CONFIGS.mapWidth, CONFIGS.mapHeight);
     
-    // add the background
+    // background
     game.add.tileSprite(0, 0, game.world.width, game.world.height, 'sky');
     game.add.tileSprite(0, 0, game.world.width, game.world.height - CONFIGS.skyHeight, 'space');
 
-    // add planets
+    // planets
     game.add.sprite(CONFIGS.mapWidth / 2, CONFIGS.mapHeight - 2 * CONFIGS.planetDistance, 'mercury');
     game.add.sprite(CONFIGS.mapWidth / 2, CONFIGS.mapHeight - 3 * CONFIGS.planetDistance, 'venus');
     game.add.sprite(CONFIGS.mapWidth / 2, CONFIGS.mapHeight - 4 * CONFIGS.planetDistance, 'mars');
@@ -71,12 +72,12 @@ function create() {
     game.add.sprite(CONFIGS.mapWidth / 2, CONFIGS.mapHeight - 10 * CONFIGS.planetDistance, 'sun');
     game.add.sprite(CONFIGS.mapWidth / 2, CONFIGS.mapHeight - 11 * CONFIGS.planetDistance, 'moon');
     
-    // create the ground
+    // ground
     ground = game.add.tileSprite(0, game.world.height - CONFIGS.groundHeight, game.world.width, CONFIGS.groundHeight, 'ground');
     game.physics.enable(ground, Phaser.Physics.ARCADE);
     ground.body.immovable = true;
     
-    // create the rocket
+    // rocket
     rocket = game.add.sprite(CONFIGS.mapWidth / 2, game.world.height - 75, 'rocket');
     rocket.anchor.set(0.5);
     game.physics.enable(rocket, Phaser.Physics.ARCADE);
@@ -87,13 +88,13 @@ function create() {
     rocket.body.setCircle(10, 15, 15);
     rocket.animations.add('rotation', makeArray(16), 12, true);
     
-    // create controls
+    // controls
     cursors = game.input.keyboard.createCursorKeys();
 
     // camera following
     game.camera.follow(rocket);
 
-    // add asteroids on the map 
+    // asteroids
     asteroids = game.add.group();
     asteroids.enableBody = true;
     for (let i = 0; i < 100; i++ ) {
@@ -103,25 +104,27 @@ function create() {
         asteroid.scale.setTo(size);
         asteroid.destroyed = false;
         asteroid.animations.add('rotation', makeArray(48), 8, true);
-        asteroid.animations.add('explosion', [49, 50, 51, 52, 53, 54, 55, 56, 57, 58], 8, false);
+        asteroid.animations.add('explosion', [48, 49, 50, 51, 52, 53, 54, 55, 56, 57], 8, false);
         asteroid.animations.play('rotation');
         //asteroid.body.gravity.y = 10;
     }
 
-    // add fuel cans on the map 
+    // fuel cans
     fuelCans = game.add.group();
     fuelCans.enableBody = true;
     for (let i = 0; i < 300; i++ ) {
         let fuelCan = fuelCans.create( game.world.randomX, game.world.randomY - 100, 'fuelCan' );
         fuelCan.body.setSize(46, 58, -9, -9);
+        fuelCan.animations.add('rotation', [0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1], 10, true);
+        fuelCan.animations.play('rotation');
     }
 
-    // create clouds
+    // clouds
     clouds = game.add.tileSprite(0, game.world.height - CONFIGS.skyHeight, game.world.width, 100, 'clouds');
     clouds.scale.setTo(2);
     clouds.anchor.set(0.5);
     
-    // create the fuelBar
+    // fuel bar
     fuelBar = new FuelBar();
     fuelBar.x = 0;
     fuelBar.y = 0;  
@@ -132,11 +135,15 @@ function create() {
     explosionSound = game.add.audio('explosionSound');
     accelerationSound = game.add.audio('accelerationSound');
 }
-function update() {
 
+function update() {
+    window.console.log(rocket.body.velocity.getMagnitude());
     // animations
-    rocket.animations.play('rotation');
-    clouds.x += 0.3;
+    if (rocket.body.velocity.getMagnitude() > 50) {
+        rocket.animations.play('rotation');
+    }
+    clouds.x += 0.4;
+
    
     // collisions
     game.physics.arcade.collide(rocket, ground);
@@ -189,9 +196,9 @@ function destroyAsteroid(rocket, asteroid) {
 }
 
 function render() {
-    game.debug.body(rocket);
-    asteroids.children.forEach( asteroid => game.debug.body(asteroid));
-    fuelCans.children.forEach( fuelCan => game.debug.body(fuelCan));
+    // game.debug.body(rocket);
+    // asteroids.children.forEach( asteroid => game.debug.body(asteroid));
+    // fuelCans.children.forEach( fuelCan => game.debug.body(fuelCan));
 }
 
 class FuelBar extends Phaser.Group {
@@ -209,7 +216,7 @@ class FuelBar extends Phaser.Group {
         this.bar.scale.setTo(this.fuelAmount, 2);
     } 
     increaseFuel() {
-        this.fuelAmount += 10;
+        this.fuelAmount += CONFIGS.fuelIncreaseAmount;
         if (this.fuelAmount > 80) {
             this.fuelAmount = 80;
         }
